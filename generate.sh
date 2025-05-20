bbb764b29db864536b3acbbd6281aeec18975ac7
Akio Kobayashi <a-kobayashi@a.tsukuba-tech.ac.jp>
2025-05-20 15:36:45 +0900

    add files

---
+++ generate.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+python3 generate.py --csv val.csv --ckpt /media/akio/hdd1/rvc/models/test/logs/ckpt/last.ckpt \
+--out_dir /media/akio/hdd1/rvc/models/test/logs/outs --stats stats_pitch.pt \
