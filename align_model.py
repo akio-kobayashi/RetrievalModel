619bbdc9da85c631fe693cfb52ef86f689d503ef
Akio Kobayashi <a-kobayashi@a.tsukuba-tech.ac.jp>
2025-05-30 18:37:12 +0900

    minor revision

--- align_model.py
+++ align_model.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import pytorch_lightning as pl
-from soft_dtw import SoftDTW
+from kornia.losses import SoftDTW
 
 # ----------------------------------------------------------------
 # Diagonal mask function (VoiceTransformer style)
